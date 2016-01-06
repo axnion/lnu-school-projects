9891acab0a06fe6c99126d1a3782645475f24969
Axel Nilsson <an222yp@student.lnu.se>
2016-01-06 11:36:08 +0100

    Moved window functionallity to appWindow.js

---
+++ examination/client/source/js/appWindow.js
@@ -0,0 +1,70 @@
+var lastIndex = 0;
+var offsetX = 0;
+var offsetY = 0;
+var positionX = 0;
+var positionY = 0;
+var element;
+
+function grabElement(target) {
+    element = target;
+    offsetX = positionX - element.offsetLeft;
+    offsetY = positionY - element.offsetTop;
+    lastIndex += 1;
+    element.style.zIndex = lastIndex;
+}
+
+function moveElement(event) {
+    positionX = event.clientX;
+    positionY = event.clientY;
+    if (element) {
+        element.style.left = positionX - (offsetX + 2) + "px";
+        element.style.top = positionY - (offsetY + 2) + "px";
+    }
+}
+
+function releaseElement() {
+    element = undefined;
+}
+
+function addTemplate(templateName, containerName) {
+    var container;
+    var template;
+    var node;
+
+    container = document.querySelector(containerName);
+    template = document.querySelector(templateName);
+    node = document.importNode(template.content, true);
+    container.appendChild(node);
+}
+
+function printWindow() {
+    var topbar;
+    var appWindow;
+
+    addTemplate("#appWindowTemplate", "body");
+
+    appWindow = document.querySelectorAll(".appWindow")[document.querySelectorAll(".appWindow").length - 1];
+    topbar = appWindow.querySelector(".topbar");
+
+    lastIndex += 1;
+    appWindow.style.zIndex = lastIndex;
+
+    topbar.addEventListener("mousedown", function() {
+        grabElement(appWindow);
+    });
+
+    appWindow.addEventListener("mousemove", moveElement);
+    document.addEventListener("mouseup", releaseElement);
+
+    appWindow.addEventListener("click", function(event) {
+        event.stopPropagation();
+        lastIndex += 1;
+        appWindow.style.zIndex = lastIndex;
+    });
+
+    topbar.querySelector(".closeWindowButton").addEventListener("click", function() {
+        appWindow.remove();
+    });
+}
+
+module.exports.printWindow = printWindow;
