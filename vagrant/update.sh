e4b1ff1cc08a6b359667f96939f46e638c8e355b
Axel Nilsson <an222yp@student.lnu.se>
2016-10-20 16:46:50 +0200

    Bundle install now works on vagrant machine

--- vagrant/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get -y upgrade
-apt-get -y remove ruby
-apt-get -y install git-core curl build-essential g++ libssl-dev libreadline-dev zlib1g-dev postgresql memcached libpq-dev nodejs libqt4-dev libqtwebkit-dev qt4-qmake 
