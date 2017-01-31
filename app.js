f8ab871f2eb3e477d939b6ca35d5e0c295209541
axnion <an222yp@student.lnu.se>
2017-01-31 16:36:44 +0100

    Presents suitable movie and restaurant bookings. Base requirements are fulfilled

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const calendarScraper = require("./lib/calendar")
 const cinemaScraper = require("./lib/cinema")
 const restaurantScraper = require("./lib/restaurant")
 
+// TODO: Go though all promises and check so they are needed. Remove any unnessecary
 // TODO: Validate argument so it's a valid URI
 // TODO: Check so the protocol is specified. HTTP should always be included
 let frontPage = process.argv[2]
@@ -31,10 +32,17 @@ calendarScraper.getLinks(frontPage).then(function(links) {
     return cinemaScraper.findMovies(cinemaURL, dates)
 }).then(function(availableMovies) {
     movies = availableMovies
-    return restaurantScraper.getAvailableBookings(restaurantURL)
-}).then(function(availableBookings) {
-    bookings = availableBookings
-    console.dir(bookings)
+    return restaurantScraper.getSuitableBookings(restaurantURL, movies)
+}).then(function(suitableBookings) {
+    presentBookings(suitableBookings)
 }).catch(function(error) {
     console.log(error)
 })
+
+function presentBookings(bookings) {
+    for (let i = 0; i < bookings.length; i++) {
+        console.log("Alternative #" + (i + 1))
+        console.log("Movie: " + bookings[i].movie.movie)
+        console.log("Restaurant booking: " + bookings[i].booking.from + ":00 to " + bookings[i].booking.to + ":00\n")
+    }
+}
