ce5b73a767ead4f2c0cb31057e3b149c4310cec2
axnion <an222yp@student.lnu.se>
2017-02-05 18:20:12 +0100

    Cleaned up app.js and added validation to user input in askForBookingTable

--- app.js
+++ app.js
@@ -1,69 +1,50 @@
 "use strict"
 
-const request = require("request-promise")
+const calendar = require("./lib/calendar")
+const cinema = require("./lib/cinema")
+const restaurant = require("./lib/restaurant")
+const presentation = require("./lib/presentation")
+const validate = require("./lib/validate")
 
-const calendarScraper = require("./lib/calendar")
-const cinemaScraper = require("./lib/cinema")
-const restaurantScraper = require("./lib/restaurant")
-
-// TODO: Write documentation on how to run the application
-let frontPage = process.argv[2]
-
+let url = process.argv[2]
 let calendarURL
 let cinemaURL
 let restaurantURL
 
-validateURL(frontPage).then(function() {
-
-    return calendarScraper.getLinks(frontPage)
+validate.url(url).then(function() {
+    // Get links to calendar, cinema, and restaurant from the front page
+    return calendar.getLinks(url)
 }).then(function(links) {
+    // Store links to global variables
     calendarURL = links[0]
     cinemaURL = links[1]
     restaurantURL = links[2]
 
-    return calendarScraper.getLinks(calendarURL)
+    // Get links to the calendar page of each person
+    return calendar.getLinks(calendarURL)
 }).then(function(links) {
-    return calendarScraper.getAllFriends(links)
+    // Get all friends and their what their status is on each day
+    return calendar.getAllFriends(links)
 }).then(function(friends) {
-    return cinemaScraper.findMovies(cinemaURL, calendarScraper.findSuitableDates(friends))
+    // Get the movies which are shown on the days where all friends are available
+    return cinema.findMovies(cinemaURL, calendar.findSuitableDates(friends))
 }).then(function(movies) {
-    return restaurantScraper.getSuitableBookings(restaurantURL, movies)
+    // Match movies and table bookings. Return object containing a suggestion with a movie and an
+    // available table.
+    return restaurant.getSuitableBookings(restaurantURL, movies)
 }).then(function(bookings) {
-    presentAlternatives(bookings)
-    return restaurantScraper.askForBookingTable(bookings)
+    // Present the alternatives to the user and ask for input on which should be booked.
+    presentation.presentAlternatives(bookings)
+    return restaurant.askForBookingTable(bookings)
 }).then(function(finalBooking) {
-    return restaurantScraper.bookTable(finalBooking, restaurantURL)
+    // Book the table choosen by the user
+    return restaurant.bookTable(finalBooking, restaurantURL)
 }).then(function(finalBooking) {
-    presentFinalBooking(finalBooking)
+    // Present the final booking and exit
+    presentation.presentFinalBooking(finalBooking)
     process.exit()
 }).catch(function(error) {
+    // Display error in console and exit
     console.log(error)
+    process.exit()
 })
-
-function presentAlternatives(bookings) {
-    for (let i = 0; i < bookings.length; i += 1) {
-        console.log("Alternative #" + (i + 1))
-        console.log("Day: " + bookings[i].day)
-        console.log("Movie: " + bookings[i].movie.title)
-        console.log("Restaurant booking: " + bookings[i].table.from + ":00 to " +
-            bookings[i].table.to + ":00\n")
-    }
-}
-
-function presentFinalBooking(booking) {
-    console.log("Table has been booked from " + booking.table.from + " to " + booking.table.to)
-}
-
-function validateURL(url) {
-    return new Promise(function(resolve, reject) {
-        if (url.substr(0, 4) !== "http") {
-            reject("Could not understand protocol. Please specify the protocol in the URL (http://)")
-        }
-
-        request(url).then(function() {
-            resolve()
-        }).catch(function(error) {
-            reject("Problem with URL\n" + error)
-        })
-    })
-}
