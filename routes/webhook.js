621552dc344d2ad750cf86f53fbbba3f8bec4a05
Axel Nilsson <an222yp@student.lnu.se>
2017-02-27 09:19:40 +0100

    Created simple notifications when creating issues

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,12 +3,26 @@
 const router = require("express").Router()
 
 router.route("/").post(function(req, res) {
+    console.log("NOTIFICATION!")
+
     let io = req.app.get("socketio")
     let test = ["Back", "blalba", "bla"]
-    io.emit("test", test)
+
+    io.sockets.emit("notification", createNotification(req.body))
 
     console.log("I'm hooked!")
     res.redirect("/")
 })
 
+function createNotification(data) {
+    return {
+        action: data.action,
+        title: data.issue.title,
+        url: data.issue.url,
+        creator: data.issue.user.login
+    }
+}
+
 module.exports = router
+
+//91jgr8u3jgr98ijg29j
