f64fca91778ca7fb0dbf108cc0c62c6518bdfa07
Axel Nilsson <an222yp@student.lnu.se>
2017-10-04 17:20:33 +0200

    Vagrant: Changed so shared folder is used in /vagrant instead of copying to /home/vagrant

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -3,6 +3,5 @@ apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
-cp -a /vagrant/* /home/vagrant
-chown -R vagrant:vagrant /home/vagrant
-cd /home/vagrant
+chown -R vagrant:vagrant /vagrant
+echo cd /vagrant >> .bashrc
