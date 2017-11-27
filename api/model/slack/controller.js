617f6ee1aba0c778162055ec942ce8f7e86ce634
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-27 18:57:46 +0100

    Remove old api

--- api/model/slack/controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller');
-const slackFacade = require('./facade');
-
-class SlackController extends Controller {}
-
-module.exports = new SlackController(slackFacade);
