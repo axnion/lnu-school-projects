3341e39eed82f979eb0ab6b69f0eba857ae374c1
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-29 21:00:55 +0100

    Accepts post to /reportexam (should come from jenkins when the student makes a release), which saves build result to mongo. Post to /bookexam checks if the build was ok and creates a response

--- api/model/bookexam/controller.js
+++ api/model/bookexam/controller.js
@@ -1,26 +1,43 @@
 const Controller = require('../../lib/controller');
 const slackFacade = require('./facade');
 const reportExamController = require('../reportexam/controller');
+const ReportExamFacade = require('../reportexam/facade');
 
 class SlackController extends Controller {
   createBooking(req, res, next) {
-    // get available exams
-    // course
-    // id
-    // buildOk
-    // exam - typ namn på repot fw222ek-examination...
+    console.log(req.body.payload); // don't delete yet
 
-    console.log(req.body.payload);
+    const response = JSON.parse(req.body.payload);
+    const studentId = response.user.name;
+    const course = response.channel.name;
+    // TODO: add exam
 
-    let response = JSON.parse(req.body.payload);
-    console.log(response.user.name);
-    const username = response.user.name;
-    const courseName = response.channel.name;
+    ReportExamFacade.findOne({ studentId, course /* plus exam */ })
+      .then(resp => {
+        console.log(resp);
 
-    res.status(200).json({
-      Hej: 'ho'
-    });
+        if (resp.buildOk) {
+          // build was ok, make reservation
+          return res.status(200).json(format(resp));
+        } else {
+          // build failed, return appropriate message
+          res.status(200).json({
+            text: 'Nope'
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
+}
+
+function format(examDoc) {
+  console.log(examDoc);
+  const formated = {
+    text: 'This is a meaningful response',
+    attachments: []
+  };
+
+  return formated;
 }
 
 module.exports = new SlackController(slackFacade);
