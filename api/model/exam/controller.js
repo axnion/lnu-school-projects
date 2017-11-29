ae58fd7e40f41ed4252f7365345d820ae3d592e3
AndrasBalla <ab223sq@student.lnu.se>
2017-11-29 17:30:33 +0100

    Implemented a list of examns

--- api/model/exam/controller.js
+++ api/model/exam/controller.js
@@ -4,7 +4,7 @@ const examFacade = require('./facade');
 class ExamController extends Controller {
 
     create(req, res, next) {
-        const timeTable = ["08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "13:00", "13:30",
+        const timeTable = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "13:00", "13:30",
             "14:00", "14:30", "15:00", "15:30", "16:00", "16:00", "17:00"];
         let exam = {
             course: req.body.channel_name,
@@ -23,7 +23,7 @@ class ExamController extends Controller {
                     endTime: timeTable[i + 1]
             }});
         }
-
+        console.log(exam);
         this.facade.create(exam)
             .then(doc => res.status(201).json(format(doc)))
             .catch(err => next(err));
@@ -31,31 +31,31 @@ class ExamController extends Controller {
 }
 
 function format(examDoc) {
-    return {
+    let formated = {
         "text": "Exam for the course: " + examDoc.course + " on " + examDoc.date ,
-        "attachments": [
-            {
-                "title": "Synopsis",
-                "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
-            },
-            {
-                "fallback": "Book your exam time",
-                "title": "Book your exam time",
-                "text": "" + examDoc.timeSlots[0].timeSlot.startTime + "-" + examDoc.timeSlots[0].timeSlot.endTime,
-                "callback_id": "comic_1234_xyz",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                    {
-                        "name": "book",
-                        "text": "Sex, Drugs & Rock&Roll",
-                        "type": "button",
-                        "value": "book"
-                    }
-                ]
-            }
-        ]
+        "attachments": []
     };
+
+    for(let i = 0; i < examDoc.timeSlots.length - 1; i++){
+        formated.attachments.push({
+            "fallback": "Book your exam time",
+            "title": "Book your exam time",
+            "text": "" + examDoc.timeSlots[i].timeSlot.startTime + "-" + examDoc.timeSlots[i + 1].timeSlot.endTime,
+            "callback_id": "comic_1234_xyz",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "book",
+                    "text": "Sex, Drugs & Rock&Roll",
+                    "type": "button",
+                    "value": "book"
+                }
+            ]
+        });
+    }
+
+    return formated;
 }
 
 module.exports = new ExamController(examFacade);
