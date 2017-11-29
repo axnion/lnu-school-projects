ecd56ebd65379e557ab2bd98d6a2ab672d45ac5c
AndrasBalla <ab223sq@student.lnu.se>
2017-11-29 21:28:39 +0100

    Added todo

--- api/model/exam/controller.js
+++ api/model/exam/controller.js
@@ -2,7 +2,7 @@ const Controller = require('../../lib/controller');
 const examFacade = require('./facade');
 
 class ExamController extends Controller {
-
+    //TODO Add a unique exam name like examination-3
     createExam(req, res, next) {
         const timeTable = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
             "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
