2232f740649cb0ff515830a508ae90548951bf25
AndrasBalla <ab223sq@student.lnu.se>
2017-12-10 16:48:16 +0100

    Removed example route

--- api/model/example/controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller');
-const exampleFacade = require('./facade');
-
-class ExampleController extends Controller {}
-
-module.exports = new ExampleController(exampleFacade);
