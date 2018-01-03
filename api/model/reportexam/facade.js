62bd00ab07c70f2ee504ebb4b6edce7ece2c4575
AndrasBalla <ab223sq@student.lnu.se>
2018-01-03 17:13:55 +0100

    Implemented instert/update for exam raport

--- api/model/reportexam/facade.js
+++ api/model/reportexam/facade.js
@@ -3,15 +3,17 @@ const reportExamSchema = require('./schema');
 
 class ReportExamFacade extends Facade {
   createExamReport(body) {
-    const schema = new this.Schema({
-      course: body.course,
-      studentId: body.studentId,
-      buildOk: body.buildOk,
-      exam: body.exam
-    });
-
-    // TODO: update istf save
-    return schema.save();
+    return reportExamSchema.update({
+            course: body.course,
+            studentId: body.studentId,
+            exam: body.exam,
+        },
+        {$set: {
+                course: body.course,
+                studentId: body.studentId,
+                buildOk: body.buildOk,
+                exam: body.exam
+            }}, { upsert: true}).exec()
   }
 }
 
