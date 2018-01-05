3b648a4aacdab9852785c7e60ae14ef9e3b542fb
AndrasBalla <ab223sq@student.lnu.se>
2018-01-05 16:23:13 +0100

    debugging staging

--- api/routes.js
+++ api/routes.js
@@ -13,6 +13,8 @@ router.route('/').get((req, res) => {
 router.route('*').all(function (req, res, next) {
     // TODO: Only check this on the slack routes
     console.log(process.env.SLACKTOKEN);
+    console.log(process.env.SLACKTOKEN !== req.body.token);
+    console.log(req.body.token);
     if(req.method === "POST" && req.url !== "/reportexam" && process.env.SLACKTOKEN !== req.body.token){
         return res.status(403);
     }
