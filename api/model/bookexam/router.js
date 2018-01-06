70f856d07b95b70e34ec1cf5941c963921d93272
wallstedt <fredrik.wallstedt@gmail.com>
2018-01-06 20:03:22 +0100

    Comment out unnecessary paths in bookexam routes

--- api/model/bookexam/router.js
+++ api/model/bookexam/router.js
@@ -4,13 +4,13 @@ const router = new Router();
 
 router
   .route('/')
-  .get((...args) => controller.find(...args))
+  /* .get((...args) => controller.find(...args)) */
   .post((...args) => controller.createBooking(...args));
 
-router
+/* router
   .route('/:id')
   .put((...args) => controller.update(...args))
   .get((...args) => controller.findById(...args))
-  .delete((...args) => controller.remove(...args));
+  .delete((...args) => controller.remove(...args)); */
 
 module.exports = router;
