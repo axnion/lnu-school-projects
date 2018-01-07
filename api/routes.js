3c959a952d0936d49cfdc1be92d045b789e5429e
Axnion <an222yp@student.lnu.se>
2018-01-07 09:03:47 +0100

    Debugging deployment versioning

--- api/routes.js
+++ api/routes.js
@@ -7,7 +7,7 @@ const reportExam = require('./model/reportexam/router');
 const user = require('./model/user/router');
 
 router.route('/').get((req, res) => {
-  res.json({ message: 'Welcome the slackapp API! 4.0' });
+  res.json({ message: 'Welcome the slackapp API! 5.0' });
 });
 
 router.route('*').all(function (req, res, next) {
