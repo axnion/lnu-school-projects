f51d3819a3dc276314d59ca48a3521139286512d
wallstedt <fredrik.wallstedt@gmail.com>
2018-01-08 18:10:53 +0100

    Check test

--- api/test/unit_tests/isSlackAdmin.test.js
+++ api/test/unit_tests/isSlackAdmin.test.js
@@ -2,7 +2,7 @@ const sinon = require('sinon');
 const isSlackAdmin = require('../../lib/isSlackAdmin');
 
 
-describe('Test ', () => {
+describe.skip('Test ', () => {
 
   // stub a token with sinon 
 
