e305bd343c419a207bd9c095937d2683d82878ae
John Häggerud <john.haggerud@lnu.se>
2018-01-22 15:28:06 +0100

    ooops i did it again

--- api/test/unit_tests/exam.test.js
+++ api/test/unit_tests/exam.test.js
@@ -1,12 +1,12 @@
-const nock = require('nock');
-const request = require('supertest')('localhost:8080');
-const controller = require('../../model/exam/controller');
-const router = require('../../model/exam/router');
-const sinon = require('sinon');
-const mongoose = require("mongoose");
-require('sinon-mongoose');
+const nock = require('nock')
+const request = require('supertest')('localhost:8080')
+const controller = require('../../model/exam/controller')
+const router = require('../../model/exam/router')
+const sinon = require('sinon')
+const mongoose = require('mongoose')
+require('sinon-mongoose')
 
-const Exam = require('../../model/exam/facade');
+const Exam = require('../../model/exam/facade')
 
 describe('Get exam route', () => {
   test('Get something', done => {
@@ -14,55 +14,55 @@ describe('Get exam route', () => {
       .get('/exam')
       .reply(200, {
         status: 200
-      });
+      })
 
     request.get('/exam').end((err, res) => {
-      expect(res.body.status).toEqual(200);
-      done();
-    });
-  });
+      expect(res.body.status).toEqual(202)
+      done()
+    })
+  })
 
   test('test find', done => {
-    let examMock = sinon.mock(Exam);
+    let examMock = sinon.mock(Exam)
     let expectedResult = {
       status: 200,
       exam: []
-    };
-    examMock.expects('find').yields(null, expectedResult);
+    }
+    examMock.expects('find').yields(null, expectedResult)
     Exam.find(function (err, result) {
-      examMock.verify();
-      examMock.restore();
+      examMock.verify()
+      examMock.restore()
       expect(result.status).toEqual(200)
-      done();
-    });
-    done();
-  });
+      done()
+    })
+    done()
+  })
 
   test('test findById', done => {
-    let examMock = sinon.mock(Exam);
+    let examMock = sinon.mock(Exam)
     let expectedResult = {
       status: 200
-    };
-    examMock.expects('findById').yields(null, expectedResult);
+    }
+    examMock.expects('findById').yields(null, expectedResult)
     Exam.findById(function (err, result) {
-      examMock.verify();
-      examMock.restore();
-      expect(result.status).toEqual(200);
-      done();
-    });
-  });
+      examMock.verify()
+      examMock.restore()
+      expect(result.status).toEqual(200)
+      done()
+    })
+  })
 
   test('test findOne', done => {
-    let examMock = sinon.mock(Exam);
+    let examMock = sinon.mock(Exam)
     let expectedResult = {
       status: 200
-    };
-    examMock.expects('findOne').yields(null, expectedResult);
+    }
+    examMock.expects('findOne').yields(null, expectedResult)
     Exam.findOne(function (err, result) {
-      examMock.verify();
-      examMock.restore();
-      expect(result.status).toEqual(200);
-      done();
-    });
-  });
-});
+      examMock.verify()
+      examMock.restore()
+      expect(result.status).toEqual(200)
+      done()
+    })
+  })
+})
